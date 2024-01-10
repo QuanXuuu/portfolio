98b0ac1dffe6473c33a9de1b71ef7c4aac981a2e
Quan Xu <xuquan69220@gmail.com>
2024-01-10 12:25:17 +0100

    Add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-600">
+      <small className="mb-2 block text-xs">
+        &copy;2024 Quan XU. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span>About this website:</span> built with React & Next.js, Tailwind
+        CSS, Vercel hosting.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
