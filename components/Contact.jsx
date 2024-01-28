98830e6014b5db9e8862d95fbe84d7b3f8db85e5
Quan Xu <xuquan69220@gmail.com>
2024-01-28 14:55:12 +0100

    minor design change

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
   }, [inView, setActiveSection]);
 
   return (
-    <section ref={ref} id="contact" className="w-full mt-52 p-2 mb-60 ">
+    <section ref={ref} id="contact" className="w-full mt-36 md:mt-40 p-2 mb-56">
       <div className="max-w-[1240px] mx-auto">
         <h2 className="uppercase font-bold text-3xl tracking-widest text-primary-light mb-6 lg:text-4xl">
           Contact
