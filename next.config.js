727f18d78777e0c72873d1d9f6605ef6770bb004
Quan Xu <xuquan69220@gmail.com>
2024-01-31 12:48:27 +0100

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: { unoptimized: true },
+  basePath: "/portfolio",
   output: "export",
 };
 
